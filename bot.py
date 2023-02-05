125ed9f55c2b0b1bee4cf2bcdc9d984bc4bd22e5
Matthew Tang <mtang1217@gmail.com>
2023-02-05 11:47:47 -0500

    adding search and other functions

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import logging
 from scraper import *
+import webbrowser
 
 logger = logging.getLogger()
 logger.setLevel(logging.CRITICAL)
@@ -23,13 +24,28 @@ corpus_trainer = ChatterBotCorpusTrainer(chatbot)
 corpus_trainer.train('chatterbot.corpus.english')
 
 exit_conditions = (":q", "quit", "exit")
+
+print("Welcome to the Hackbot!\nA friendly chatbot that can provide video game help and search through reddit")
+print("Type \"help\" for more options")
 while True:
     query = input("> ")
     if query in exit_conditions:
         break
     elif query in "help":
-        print("HELP")
+        print("Type \"reddit\" for the top posts of your favorite subreddits!")
+        print("     \"search\" for a general search through reddit with keywords")
+        print("     \"subredditsearch\" for a search through a specific subreddit with keywords")
+        print("     \"comments\" for the top comments of a reddit post")
+        print("     \"gamehelp\" for advice on video games")
+    elif query in "gamehelp":
+        gamehelp(input("Which game? \n> "))
+    elif query in "search":
+        search(input("Input keywords \n> "))
+    elif query in "subredditsearch":
+        subsearch(input("Input subreddit and keywords in format \"Subreddit Keyword\"\n> "))
     elif query in "reddit":
-        scraper(input("which subreddit"))
+        scraper(input("which subreddit? \n> "))
+    elif query in "comments":
+        comments(input("Input URL: \n> "))
     else:
         print(f"{chatbot.get_response(query)}")
--- scraper.py
+++ scraper.py
@@ -4,12 +4,84 @@ import praw
 reddit_read_only = praw.Reddit(client_id="BADD-RyGlOxA7-G-FsIqxA",
                                client_secret="c1hpbMp_VGHThdf5ydT-IaWxP6tB3A",
                                user_agent="Mtang1217")
-def scraper(s): 
+def scraper(s):
     subreddit = reddit_read_only.subreddit(s)
     print("Display Name:", subreddit.display_name)
     print("Title:", subreddit.title)
     #print("Description:", subreddit.description)
-
+    count = 0
+    postlist = []
+    print("Top 5 Hot Posts of " + subreddit.display_name + ":")
     for post in subreddit.hot(limit=5):
-        print(post.title)
+        postlist.append(post)
+        count+=1
+        print(str(count) + ". " + post.title)
+        print(post.url)
         print()
+    result = input("If you would like to read the contents of a post, input the number of the post\nType \"comments\" if you would like to see the top comments of a post\n> ") 
+    if result.isdigit():
+        print(postlist[int(result) - 1].selftext)
+    elif result == "comments":
+        comments(input("Input URL: \n> "))
+    
+def comments(link):
+    submission = reddit_read_only.submission(url=link)
+    count = 0
+    limit = 5
+    for comment in submission.comments:
+        count+=1
+        print(str(count)+ ". " + comment.body)
+        print()
+        if count >= limit:
+            if input("Would you like more comments? \n> ").lower() == ("yes"):
+                limit += 5
+            else:
+                break
+            
+def gamehelp(game):
+    count = 0
+    submissionlist = []
+    for submission in reddit_read_only.subreddit("all").search(game + " guide"):
+        submissionlist.append(submission)
+        count+=1
+        print(str(count) + ". " + submission.title)
+        print(submission.url)
+        print()
+        if count >= 5:
+            result = input("If you would like to read the contents of a post, input the number of the post\n> ") 
+            if result.isdigit():
+                print(submissionlist[int(result) - 1].selftext)
+            break
+        
+def subsearch(keywords):
+    x = []
+    x = keywords.split()
+    count = 0
+    submissionlist = []
+    for submission in reddit_read_only.subreddit(x[0]).search(x[1]):
+        submissionlist.append(submission)
+        count+=1
+        print(str(count) + ". " + submission.title)
+        print(submission.url)
+        print()
+        if count >= 5:
+            result = input("If you would like to read the contents of a post, input the number of the post\n> ") 
+            if result.isdigit():
+                print(submissionlist[int(result) - 1].selftext)
+            break
+            
+def search(keywords):
+    count = 0
+    submissionlist = []
+    for submission in reddit_read_only.subreddit("all").search(keywords):
+        submissionlist.append(submission)
+        count+=1
+        print(str(count) + ". " + submission.title)
+        print(submission.url)
+        print()
+        if count >= 5:
+            result = input("If you would like to read the contents of a post, input the number of the post\n> ") 
+            if result.isdigit():
+                print(submissionlist[int(result) - 1].selftext)
+            break
+
